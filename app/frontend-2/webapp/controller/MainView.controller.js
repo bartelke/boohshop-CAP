e9493f7ce074e3e2ad02c2d4a2a7cea3e70c4f33
Bartosz Kloc <atlf75800@gmail.com>
2022-10-03 12:31:29 +0000

    added routing to AuthorsBooks

--- app/frontend-2/webapp/controller/MainView.controller.js
+++ app/frontend-2/webapp/controller/MainView.controller.js
@@ -1,15 +1,19 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel) {
+    "use strict";
 
-        return Controller.extend("sdk.frontend2.controller.MainView", {
-            onInit: function () {
-
-            }
+    return Controller.extend("sdk.frontend2.controller.MainView", {
+      onInit: function () {},
+      onPress: function (oEvent) {
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo("authorsBooks", {
+          authorID: oEvent.getSource().getBindingContext().getObject().ID,
         });
+      },
     });
+  }
+);
